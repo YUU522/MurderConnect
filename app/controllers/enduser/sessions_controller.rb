cfdddfa28a643f64ceba6ca4ca3ea7e7acdc643b
YUU522 <kuromu2750@gmail.com>
2023-10-25 06:06:38 +0000

    deviseの日本語化

--- app/controllers/enduser/sessions_controller.rb
+++ app/controllers/enduser/sessions_controller.rb
@@ -2,17 +2,13 @@
 
 class Enduser::SessionsController < Devise::SessionsController
   before_action :authenticate_enduser!
-  before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_permitted_parameters, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
-
-
-
   def guest_sign_in
     enduser = Enduser.guest
     sign_in enduser
     redirect_to posts_path, notice: 'ゲストユーザーとしてログインしました。'
   end
-
 
   # GET /resource/sign_in
   # def new
@@ -32,7 +28,9 @@ class Enduser::SessionsController < Devise::SessionsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+ protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+  end
 end
